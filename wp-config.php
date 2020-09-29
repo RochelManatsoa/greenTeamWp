7f42cbad15fca1ad3a4f97a15051a5b29b49d7f1
tojo2001 <tojorandria474@gmail.com>
2020-09-29 16:50:56 +0200

    initiale commite

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define( 'DB_NAME', 'greentem_wp' );
 
 /** MySQL database username */
-define( 'DB_USER', 'greentem_wp' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'x)6pS1S-6M' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
